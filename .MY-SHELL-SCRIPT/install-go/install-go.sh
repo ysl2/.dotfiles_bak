cd55efe7f6cd3631cacf730e4a430bce9cd420ab
ysl2 <songli.yu@outlook.com>
2021-08-02 20:19:44 +0800

    Modify install go script

--- .MY-SHELL-SCRIPT/install-go/install-go.sh
+++ .MY-SHELL-SCRIPT/install-go/install-go.sh
@@ -1,10 +1,11 @@
 #/usr/bin/bash
 
-mkdir -p assets
-cd assets
+mkdir -p temp
+cd temp
 
-curl https://mirrors.ustc.edu.cn/golang/go1.16.5.linux-amd64.tar.gz --remote-name
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf $(ls .)
+# curl https://mirrors.ustc.edu.cn/golang/go1.16.5.linux-amd64.tar.gz --remote-name
+# sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf $(ls .)
+wget -c https://mirrors.ustc.edu.cn/golang/go1.16.5.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local
 
 cd -
-rm -rf assets
+# rm -rf assets
