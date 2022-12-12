fc649732108a34822add1133a2d40812b3921107
ysl2 <ysl2@88.com>
2022-12-13 00:00:59 +0800

    1

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -54,7 +54,13 @@ require('packer').startup(
     function(use)
       use 'wbthomason/packer.nvim'
       use {'neoclide/coc.nvim', branch = 'release'}
-      use 'nvim-treesitter/nvim-treesitter'
+      use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+          local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+          ts_update()
+        end,
+      }
       use 'easymotion/vim-easymotion'
       use 'tpope/vim-surround'
       use 'tpope/vim-commentary'
@@ -64,6 +70,20 @@ require('packer').startup(
       use 'Asheq/close-buffers.vim'
       use 'numirias/semshi'
       use 'jbgutierrez/vim-better-comments'
+      use 'luochen1990/rainbow'
+      use 'nvim-tree/nvim-web-devicons'
+      -- {
+      use {
+          'nvim-telescope/telescope.nvim', 
+          tag = '0.1.0',
+          requires = { {'nvim-lua/plenary.nvim'} }
+      }
+      use {
+        'nvim-telescope/telescope-fzf-native.nvim', 
+        run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' 
+      }
+      -- }
+      use 'mg979/vim-xtabline'
 
       -- Automatically set up your configuration after cloning packer.nvim
       -- Put this at the end after all plugins
@@ -86,6 +106,51 @@ for _, p in pairs(parsers) do
     'git@git.zhlh6.cn:'
   )
 end
+
+require('nvim-treesitter.configs').setup {
+  -- A list of parser names, or "all"
+  ensure_installed = { 'lua', 'bash', 'bibtex', 'git_rebase', 'gitattributes', 'gitcommit', 'gitignore', 'diff', 'help', 'http', 'json', 'jsonc', 'latex', 'lua', 'markdown', 'markdown_inline', 'python', 'regex', 'toml', 'vim', 'yaml'},
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = true,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
+
+  -- List of parsers to ignore installing (for "all")
+  -- ignore_install = { "javascript" },
+  ignore_install = {},
+
+  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+  highlight = {
+    -- `false` will disable the whole extension
+    enable = true,
+
+    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+    -- the name of the parser)
+    -- list of language that will be disabled
+    -- disable = { "c", "rust" },
+    disable = {},
+    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+    -- disable = function(lang, buf)
+    --     local max_filesize = 100 * 1024 -- 100 KB
+    --     local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+    --     if ok and stats and stats.size > max_filesize then
+    --         return true
+    --     end
+    -- end,
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
 
 -- ===
 -- === neoclide/coc.nvim
@@ -305,4 +370,16 @@ vim.g.lightline = { colorscheme =  'wombat', }
 -- === kdheepak/lazygit.nvim
 -- ===
 vim.keymap.set('n', [[\g]], ':LazyGit<CR>', {silent = true, noremap = true})
+
+-- ===
+-- === nvim-tree/nvim-web-devicons
+-- ===
+require'nvim-web-devicons'.setup {
+  default = true;
+}
+
+-- ===
+-- === nvim-telescope/telescope-fzf-native.nvim
+-- ===
+require('telescope').load_extension('fzf')
 
